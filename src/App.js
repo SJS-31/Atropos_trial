15d59750b96498996fc0a90f8fbbbd3abd4b6218
SJS-31 <rajswapnil31@gmail.com>
2023-06-05 23:41:47 +0530

    Playing with Atropos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'atropos/css'
+import Astro from './Astro.jpg'
+import bgvideo from './video.mp4'
+import Atropos from 'atropos/react';
 
-function App() {
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div id="app"> 
+    <div className="card">
 
-export default App;
+      {/* <video autoplay loop muted plays-inline class="back-video" >
+        <source src={bgvideo} type="video/mp4"/>
+      </video> */}
+      <Atropos className="my-atropos"
+        activeOffset={40}
+        shadowScale={1.05}
+        onEnter={() => console.log('Enter')}
+        onLeave={() => console.log('Leave')}
+        onRotate={(x, y) => console.log('Rotate', x, y)}
+      >
+        <img src={Astro} data-atropos-offset="-5" className="App-logo" alt="logo" />
+        <h1 data-atropos-offset="5">WEB3 SUMMIT</h1>
+        <button id="btn" data-atropos-offset="5" >Register</button>
+      
+      </Atropos>
+      </div>
+    </div>
+  )
+}
